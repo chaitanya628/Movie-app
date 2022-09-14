b1e8ee12351922fb2a3498ca79837b79547c7887
chaitanya628 <chaitanya.chandwani1699@gmail.com>
2022-09-14 13:59:24 +0530

    Checkpoint 1 & 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <div className='header-img'></div>
+      </div>
+    )
+  }
+}
